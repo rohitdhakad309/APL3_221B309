4719d347e51070457a4842febe8dbee7b6ec62f7
221B309 <221B309@juet.co.in>
2025-01-22 15:17:02 +0530

    add

---
+++ le1/q1/class Mother.java
@@ -0,0 +1,24 @@
+class Mother {
+    void show() {
+        System.out.println("Hello World");
+    }
+}
+
+
+class Child extends Mother {
+    @Override
+    void show() {
+        System.out.println("Hello JUET");
+    }
+}
+
+
+public class q2 {
+    public static void main(String[] args) {
+        Mother obj1 = new Mother();
+        obj1.show(); 
+
+        Child obj2 = new Child();
+        obj2.show(); 
+    }
+}
--- le1/q2/class Mother.java
+++ le1/q2/class Mother.java
@@ -13,7 +13,7 @@ class Child extends Mother {
 }
 
 
-public class Overriding {
+public class Main {
     public static void main(String[] args) {
         Mother obj1 = new Mother();
         obj1.show(); 
