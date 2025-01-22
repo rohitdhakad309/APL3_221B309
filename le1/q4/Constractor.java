62c58a2bb5ed6063f56e57cf33f5c74ebb0e09a2
221B309 <221B309@juet.co.in>
2025-01-22 15:23:50 +0530

    add

---
+++ le1/q4/Constractor.java
@@ -0,0 +1,24 @@
+class One {
+    //base class
+    One(int x) {
+        System.out.println("Constructor of One : " + x);
+    }
+}
+
+
+class Two extends One { //child class
+    
+    Two(int x) {
+        super(x); // calling x
+        
+                System.out.println("Constructor of Two" );
+    }
+}
+
+
+public class Constractor {
+    public static void main(String[] args) {
+        
+        Two obj = new Two(10);
+    }
+}
