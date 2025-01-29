1e88c99de21dbefc39c46624e82fd85297bf4050
221B309 <221B309@juet.co.in>
2025-01-29 15:46:06 +0530

    files

---
+++ LAB 2/Main.java
@@ -0,0 +1,47 @@
+class Animal{
+    void hear(){
+
+    }
+}
+class Cow extends Animal{
+    void hear(){
+        System.out.println("cow : moo");
+    }
+}
+class Dog extends Animal{
+    void hear(){
+        System.out.println("dog : bark");
+    }
+}
+class Goat extends Animal{
+    void hear(){
+        System.out.println("goat : meeeeee");
+    }
+}
+class Horse extends Animal{
+    void hear(){
+        System.out.println("horse : hoooo");
+    }
+}
+class Voice{
+    Animal[] animals = new Animal[5];
+     void preparevoide(){
+        animals[0] = new Cow();
+        animals[1] = new Dog();
+        animals[2] = new Goat();
+        animals[3] = new Horse();
+     }
+     Void hear(){
+        for(Animal animal : animals){
+            animal.hear();
+        }
+     }
+}
+
+public class Main {
+    public static void Main(String[] args){
+    Voice voice = new Voice();
+    voice.templateMethod();
+    }
+
+}
